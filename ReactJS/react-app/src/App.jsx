4442bdc927fb7ec06ae7d5eb3c02f3beec19aa43
GIRISH-VV <girishkumarvv19@gmail.com>
2026-01-08 18:58:12 +0530

    hookssortfilterclick

--- ReactJS/react-app/src/App.jsx
+++ ReactJS/react-app/src/App.jsx
@@ -1,17 +1,15 @@
 import './App.css'
-import Products from './Products'
-import HeadPhone from './assets/HeadPhone.jpg'    
-import Glass from './assets/Glass.jpg'    
-import Shoe from './assets/Shoe.jpg'    
-import Watch from './assets/Watch.jpg'
+import ProductList from './ProductList.jsx'
+
 
 function App() {
   return (
     <>
-      <Products img={HeadPhone} name="HeadPhone" price="$1200" />
+      {/* <Products img={HeadPhone} name="HeadPhone" price="$1200" />
       <Products img={Glass} name="Glass" price="$900" />
       <Products img={Shoe} name="Shoe" price="$700" />
-      <Products img={Watch} name="Watch" price="$1100" />
+      <Products img={Watch} name="Watch" price="$1100" /> */}
+      <ProductList/>
     </>
   )
 }

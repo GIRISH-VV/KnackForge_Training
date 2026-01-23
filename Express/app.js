25c34011a726f9b15fb9f84fc02578631173f603
GIRISH-VV <girishkumarvv19@gmail.com>
2026-01-23 18:35:05 +0530

    newtopics

--- Express/app.js
+++ Express/app.js
@@ -3,6 +3,10 @@ const app = express()
 
 import cookieParser from 'cookie-parser'
 import session from 'express-session'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import products from './data/products.js'
+import {query,validationResult} from 'express-validator'
 
 app.use(cookieParser())         //MW - Parses req.cookie
 app.use(session({
@@ -47,6 +51,7 @@ app.get('/remove-visit',(req,res)=>{
     res.send("Session Removed")
 })
 
+//Session based Authentication
 app.post('/register',async (req,res) =>{
     const {username,password} = req.body
     users.push({
@@ -75,8 +80,78 @@ app.get('/dashboard',(req,res) => {
     res.send(`Welcome , ${req.session.user.username}`)
 })
 
+//JWT based Authentication
+
+app.post('/register1',async (req,res) =>{
+    const {username,password} = req.body
+    const hashedPassword = await bcrypt.hash(password,10)
+    users.push({
+        username,
+        password:hashedPassword
+    })
+    res.send('User Registered')
+})
+
+app.post('/login1',async (req,res) =>{
+    const {username,password} = req.body
+    const user = users.find(u=> u.username === username)
+    if(!user || !(await bcrypt.compare(password,user.password))){
+        return res.send('Not authorized')
+    }
+    const token = jwt.sign({username},'test#secret')
+    res.json({token})
+}) 
+
+app.get('/dashboard1',(req,res) => {
+    try {
+        const token = req.header("Authorization")
+    const decodedToken = jwt.verify(token,'test#secret')
+    if(decodedToken.username){
+        res.send(`Welcome, ${decodedToken.username}`)
+    }
+    else{
+        res.send('Acces Denied')
+    }
+    } catch (error) {
+        res.send('Acces Denied')
+    }
+})
 
 
+//RESTful API
+
+app.get('/api/products',(req,res)=>{
+    // const products = [
+    //     {id:1,name:'Laptop',price:2000},
+    //     {id:2,name:'Mobile',price:1000}
+    // ]
+    res.status(200).json({products})
+})
+
+app.get('/api/products/:id',(req,res)=>{
+    // const products = [
+    //     {id:1,name:'Laptop',price:2000},
+    //     {id:2,name:'Mobile',price:1000}
+    // ]
+    const product = products.find( p=> Number(req.params.id) === p.id)
+    if(!product){
+       return res.status(400).json({message:'Product not Found'})
+    }
+    res.status(200).json({product})
+})
+
+app.post('/api/products',(req,res)=>{
+    const newProduct = req.body
+    newProduct.id = Date.now()
+    res.status(201).json({newProduct})
+})
+
+//Validaton & Sanitization
+
+app.get('/valid',query('uname').escape().trim().notEmpty().withMessage('name is Empty'),(req,res) =>{
+    const result = validationResult(req)
+    res.send(result)
+})
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
---
+++ Express/data/products.js
@@ -0,0 +1,15 @@
+
+const products = [
+    {
+    id:1,
+    name:'Laptop',
+    price:2000
+    },
+    {
+        id:2,
+        name:'Mobile',
+        price:1000
+    }
+]
+
+export default products
